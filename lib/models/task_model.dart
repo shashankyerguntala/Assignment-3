f6b97f97ced11a988fbfded3e9bad4c8bb9bad76
shashankyerguntala <93209763+shashankyerguntala@users.noreply.github.com>
2025-07-28 00:26:08 +0530

    Update task_model.dart

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,11 +1,9 @@
 class TaskModel {
-  final String id;
   final String title;
   final String description;
   final String dueDate;
 
   TaskModel({
-    required this.id,
     required this.title,
     required this.description,
     required this.dueDate,
