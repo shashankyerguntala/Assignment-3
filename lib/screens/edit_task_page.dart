119de1a6eddb1372db7a4116886d0531671b198d
shashankyerguntala <93209763+shashankyerguntala@users.noreply.github.com>
2025-07-27 23:10:22 +0530

    Update edit_task_page.dart

--- lib/screens/edit_task_page.dart
+++ lib/screens/edit_task_page.dart
@@ -1,91 +1,91 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class EditTaskPage extends StatefulWidget {
-  const EditTaskPage({super.key});
+// class EditTaskPage extends StatefulWidget {
+//   const EditTaskPage({super.key});
 
-  @override
-  State<EditTaskPage> createState() => _EditTaskPageState();
-}
+//   @override
+//   State<EditTaskPage> createState() => _EditTaskPageState();
+// }
 
-class _EditTaskPageState extends State<EditTaskPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () => Navigator.pop(context),
-          icon: Icon(Icons.close),
-        ),
-        centerTitle: true,
-        title: Text(
-          'Edit Task ',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //! title start
-            const SizedBox(height: 5),
-            Container(
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 205, 232, 255),
-                borderRadius: BorderRadius.circular(10),
-              ),
+// class _EditTaskPageState extends State<EditTaskPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         leading: IconButton(
+//           onPressed: () => Navigator.pop(context),
+//           icon: Icon(Icons.close),
+//         ),
+//         centerTitle: true,
+//         title: Text(
+//           'Edit Task ',
+//           style: TextStyle(fontWeight: FontWeight.bold),
+//         ),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(15),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             //! title start
+//             const SizedBox(height: 5),
+//             Container(
+//               decoration: BoxDecoration(
+//                 color: const Color.fromARGB(255, 205, 232, 255),
+//                 borderRadius: BorderRadius.circular(10),
+//               ),
 
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10, right: 10),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Task Name ",
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
+//               child: Padding(
+//                 padding: const EdgeInsets.only(left: 10, right: 10),
+//                 child: TextField(
+//                   decoration: InputDecoration(
+//                     hintText: "Task Name ",
+//                     border: InputBorder.none,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(height: 20),
 
-            //! description start
-            Container(
-              height: (MediaQuery.of(context).size.height) / 4,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 205, 232, 255),
-                borderRadius: BorderRadius.circular(10),
-              ),
+//             //! description start
+//             Container(
+//               height: (MediaQuery.of(context).size.height) / 4,
+//               decoration: BoxDecoration(
+//                 color: const Color.fromARGB(255, 205, 232, 255),
+//                 borderRadius: BorderRadius.circular(10),
+//               ),
 
-              child: Padding(
-                padding: const EdgeInsets.only(left: 10, right: 10),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Description",
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 0, 16, 40),
-        child: SizedBox(
-          width: double.infinity,
-          child: ElevatedButton(
-            onPressed: () {},
+//               child: Padding(
+//                 padding: const EdgeInsets.only(left: 10, right: 10),
+//                 child: TextField(
+//                   decoration: InputDecoration(
+//                     hintText: "Description",
+//                     border: InputBorder.none,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       bottomNavigationBar: Padding(
+//         padding: const EdgeInsets.fromLTRB(16, 0, 16, 40),
+//         child: SizedBox(
+//           width: double.infinity,
+//           child: ElevatedButton(
+//             onPressed: () {},
 
-            style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-            child: Text(
-              'Save',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+//             style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+//             child: Text(
+//               'Save',
+//               style: TextStyle(
+//                 color: Colors.white,
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
